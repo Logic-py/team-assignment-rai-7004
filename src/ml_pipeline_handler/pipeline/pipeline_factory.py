3a03aede145c025fa41867a022bbb036d6d4e601
asyamantovani14 <asya.m@students.opit.com>
2024-12-06 23:23:15 +0100

    bux fix

--- src/ml_pipeline_handler/pipeline/pipeline_factory.py
+++ src/ml_pipeline_handler/pipeline/pipeline_factory.py
@@ -1,5 +1,9 @@
 from ..pipeline.decision_tree_classifier_pipeline import DecisionTreeClassifierPipeline
 from ..pipeline.decision_tree_regressor_pipeline import DecisionTreeRegressorPipeline
+from ..pipeline.linear_regression_pipeline import LinearRegressionPipeline
+from ..pipeline.base_config import PipelineConfig
+from ..pipeline.base_pipeline import BasePipeline
+from ..algorithm import AlgorithmType
 
 class PipelineFactory:
     """Pipeline Factory class."""
@@ -14,6 +18,8 @@ class PipelineFactory:
         Returns:
             BasePipeline object that contains a specific model type.
 
+        Raises:
+            NotImplementedError: If the specified algorithm is not supported.
         """
         if config.algorithm == AlgorithmType.LINEAR_REGRESSION:
             return LinearRegressionPipeline(config=config)
