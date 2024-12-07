eed03a54c4d123fd451884500a19ab7c70fcd405
asyamantovani14 <asya.m@students.opit.com>
2024-12-07 21:37:40 +0100

    update metric_factory

--- src/ml_pipeline_handler/metric/metric_factory.py
+++ src/ml_pipeline_handler/metric/metric_factory.py
@@ -1,38 +1,30 @@
-"""Pipeline Factory Module.
+"""Metrics Factory Module."""
 
-Questo modulo fornisce una classe Factory per costruire pipeline specifiche basate su una configurazione fornita.
-Supporta diversi algoritmi come regressione lineare, classificatori e regressori con alberi decisionali.
-"""
-from src.ml_pipeline_handler.algorithm import AlgorithmType
-from src.ml_pipeline_handler.pipeline.base_config import PipelineConfig
-from src.ml_pipeline_handler.pipeline.base_pipeline import BasePipeline
-from src.ml_pipeline_handler.pipeline.decision_tree_classifier_pipeline import DecisionTreeClassifierPipeline
-from src.ml_pipeline_handler.pipeline.decision_tree_regressor_pipeline import DecisionTreeRegressorPipeline
-from src.ml_pipeline_handler.pipeline.linear_regression_pipeline import LinearRegressionPipeline
+from loguru import logger
+
+from ..metric.base_metric_handler import BaseMetricsHandler
+from ..metric.classification.handler import ClassificationMetricHandler
+from ..metric.model_type import ModelType
+from ..metric.regression.handler import RegressionMetricHandler
 
 
-class PipelineFactory:
-    """Pipeline Factory class."""
+class MetricFactory:
+    """Metrics Factory pattern Class."""
 
     @classmethod
-    def build_pipeline(cls, config: PipelineConfig) -> BasePipeline:
-        """Build a pipeline based on the configuration.
+    def get_metrics_handler(cls, model_type: ModelType) -> BaseMetricsHandler:
+        """Instantiate the concrete Metrics Handler Class based on the model type.
 
         Args:
-            config: PipelineConfig, contains configuration information for a pipeline.
+            model_type: Type of Machine Learning Model.
 
         Returns:
-            BasePipeline object that contains a specific model type.
-
-        Raises:
-            NotImplementedError: If the specified algorithm is not supported.
+            The Concrete Metrics Handler Class based on given model type.
 
         """
-        if config.algorithm == AlgorithmType.LINEAR_REGRESSION:
-            return LinearRegressionPipeline(config=config)
-        if config.algorithm == AlgorithmType.DECISION_TREE_CLASSIFIER:
-            return DecisionTreeClassifierPipeline(config=config)
-        if config.algorithm == AlgorithmType.DECISION_TREE_REGRESSOR:
-            return DecisionTreeRegressorPipeline(config=config)
-
-        raise NotImplementedError(f"Algorithm '{config.algorithm}' is not implemented.")
+        if model_type == ModelType.REGRESSION:
+            return RegressionMetricHandler(model_type=model_type)
+        if model_type == ModelType.CLASSIFICATION:
+            return ClassificationMetricHandler(model_type=model_type)
+        logger.error(f"Model Type: {model_type} is not valid.")
+        raise NotImplementedError
