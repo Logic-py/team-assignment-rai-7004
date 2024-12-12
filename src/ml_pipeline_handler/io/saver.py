9a2c808e49beaec9dd92a914943862229334e4c5
TimNuddel <tim.d@students.opit.com>
2024-12-12 18:24:46 +0100

    fixed some issues with previous push

--- src/ml_pipeline_handler/io/saver.py
+++ src/ml_pipeline_handler/io/saver.py
@@ -2,6 +2,15 @@
 import pickle
 
 def save_model(model, file_name) -> None:
-    with open(file_name+'.pkl', 'wb') as f:
+    """Save the model as a pickle file.
+
+    Args:
+        model: The pipeline to be saved
+        file_name: The target file name, with optional directory and mandatory .pkl extension
+
+    Returns:
+        None
+
+    """
+    with open(file_name, "wb") as f:
         pickle.dump(model, f)
-    return None
