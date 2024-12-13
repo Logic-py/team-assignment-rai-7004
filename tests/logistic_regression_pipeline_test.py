88c17f96edc48a0ab7336ae3600a4082e4e99a65
Ashish R <ashish.r@students.opit.com>
2024-12-13 15:38:29 +0500

    7004 classification test (#12)
    * changes made in classification folder and also the factory_pipeline
    
    * test file directory overwrite in logistic and random forest pipeline test files
    
    * Updated the Codes with Iris.csv
    
    Will delete
    # Have been replaced - data_path="data/test_data.csv",
    # Have been replaced - features=["feature1", "feature2"],
    # Have been replaced - target_column="target",
    
    * Update pipeline_factory
    
    * Updated pipeline_factory.py with correct indentation
    
    * style: formatting project
    
    ---------
    
    Co-authored-by: Loïc Motheu <lmotheu@gmail.com>

---
+++ tests/logistic_regression_pipeline_test.py
@@ -0,0 +1,22 @@
+from src.ml_pipeline_handler.algorithm import AlgorithmType
+from src.ml_pipeline_handler.pipeline.base_config import PipelineConfig
+from src.ml_pipeline_handler.pipeline.pipeline_factory import PipelineFactory
+
+# Have been replaced - data_path="data/test_data.csv",
+# Have been replaced - features=["feature1", "feature2"],
+# Have been replaced - target_column="target",
+
+def test_logistic_regression_classifier_pipeline():
+    config = PipelineConfig(
+        data_path="data/Iris.csv",
+        features=["SepalLengthCm", "SepalWidthCm","PetalLengthCm","PetalWidthCm"],
+        target_column="Species",
+        algorithm=AlgorithmType.LOGISTIC_REGRESSION,
+        out_file="models/logistic_regression.pkl",
+        random_state=42,
+        num_folds=5,
+    )
+    pipeline = PipelineFactory.build_pipeline(config=config)
+    assert pipeline is not None
+
+
